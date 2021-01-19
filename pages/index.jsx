fc6fab545b0b6deb3a7e50c14c78c18279fca0dd
3FE 3LE (Frank Licona) <3l33f3@gmail.com>
2021-01-19 14:55:32 -0500

    stable double way translation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import { Tooltip } from '@material-ui/core';
+import copy from 'copy-to-clipboard';
 import Head from 'next/head'
 import { useState } from 'react'
 
@@ -9,7 +10,10 @@ export default function Home() {
   const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"," "];
   const chars   = ["@",";","'","$","3","_","&","-","8","+","(",")","?","!","/","9","0","1","4","#","5","7",":","2","*","6",'"'," "]
 
-
+  const ctc = ()=>{
+    copy(text)
+    console.log("copies to your clipboard ",text)
+  }
 
   const translate = (text) => {
     let newString=''
@@ -18,9 +22,10 @@ export default function Home() {
         const char = text.charAt(i);
         let newChar = char.toLowerCase();
         letters.forEach((char,i) => {
-          // console.log(newChar,char)
           newString += (newChar == char? chars[i] :"") 
-          // console.log(newString) 
+        });
+        chars.forEach((char,i) => {
+          newString += (newChar == char? letters[i] :"") 
         });
       }
     }
@@ -39,24 +44,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="w-full h-screen bg-gray-700 flex flex-wrap justify-center ">
-        <div className="p-2 w-3/4 bg-black flex flex-wrap justify-center items-center rounded-b-3xl display-flex shadow-2xl z-10">
-          <h1 className="text-9xl text-gray-200 font-black text-center" >Paste your text!!</h1>
+        <div className="p-2  w-full  xl:w-3/4 bg-black flex flex-wrap justify-center items-center rounded-b-3xl display-flex shadow-2xl z-10">
+          <h1 className="text-8xl text-gray-200 font-black text-center" >Paste your text!!</h1>
           <input
             onChange={() => handleChange(event)}
             placeholder="here!"
-            className="rounded-xl shadow-sm p-2 h-10 w-3/6 my-12 text-center "
+            className="rounded-xl shadow-sm  p-2 h-10  my-12 text-center "
           />
         </div>
         <div
-          className="p-2 w-3/4 -mt-10 bg-white flex justify-center items-center z-0"
+          className="p-2 w-full  xl:w-3/4 -mt-10 bg-white flex flex-wrap justify-center items-center z-0"
         >
           <Tooltip
-            
+            title="copy to clipboard"
+            onClick={()=>{ctc()}}
           >
-          <p 
-            ref={(p) => p}
-            value={text}
-            className="text-7xl text-center text-gray-700">
+          <p
+            className="text-2xl text-center text-gray-700"
+          >
             {text}
           </p>
           </Tooltip>
